16cbde6e82111f9e2f747567f5faf589122b68cf
mc-santiago <mari.c.santiago@europe.com>
2020-10-21 19:31:29 +0200

    part 2 solved

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ Day 1 Challenge from the Advent of Code website from 2015. <br>
 </p>
 
 --- Not Quite Lisp ---
+
 Santa was hoping for a white Christmas, but his weather machine's "snow" function is powered by stars, and he's fresh out! To save Christmas, he needs you to collect fifty stars by December 25th.
 
 Collect stars by helping Santa solve puzzles. Two puzzles will be made available on each day in the Advent calendar; the second puzzle is unlocked when you complete the first. Each puzzle grants one star. Good luck!
@@ -28,3 +29,14 @@ For example:
 To what floor do the instructions take Santa?
 
 Puzzle Input Part 1: [https://adventofcode.com/2015/day/1/input](https://adventofcode.com/2015/day/1/input)
+
+--- Part Two ---
+
+Now, given the same instructions, find the position of the first character that causes him to enter the basement (floor -1). The first character in the instructions has position 1, the second character has position 2, and so on.
+
+For example:
+
+) causes him to enter the basement at character position 1.
+()()) causes him to enter the basement at character position 5.
+
+What is the position of the character that causes Santa to first enter the basement using the same puzzle from part 1?
--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const fs = require('fs');
 
 function part1() {
    fs.readFile('./puzzle.txt', (err, data) => {
+      console.time('santa-time');
       if (err) {
          console.log(`Error! ${err}`);
       }
@@ -21,8 +22,42 @@ function part1() {
             return (acc -= 1);
          }
       }, 0);
-      console.log(`Santa should go to floor ${answer}.);
+      console.timeEnd('santa-time');
+      console.log(`Santa should go to floor ${answer}.`);
    });
 }
 
 part1();
+
+// Part 2: What is the position of the character that causes Santa to first enter the basement?
+
+function part2() {
+   fs.readFile('./puzzle.txt', (err, data) => {
+      console.time('santa-time-basement');
+      if (err) {
+         console.log(`Error! ${err}`);
+      }
+      // read directions
+      const directions = data.toString();
+      // split each direction
+      const directionsArray = directions.split('');
+      // assign what to do with each direction
+      // ( -> UP 1 floor
+      // ) -> DOWN 1 floor
+      let acc = 0;
+      let position = 0;
+      const search = directionsArray.some((cur) => {
+         if (cur === '(') {
+            acc += 1;
+         } else if (cur === ')') {
+            acc -= 1;
+         }
+         position++;
+         return acc < 0;
+      });
+      console.timeEnd('santa-time-basement');
+      console.log(`Santa entered the basement on ${position} position.`);
+   });
+}
+
+part2();
